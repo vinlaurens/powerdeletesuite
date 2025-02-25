8fd2e6c1926f8eee34cfa52b19c0f9f051855088
Vincent van den Berg <v.vandenberg@vanmeeuwen.com>
2025-02-25 18:59:04 +0100

    Adjust timer to 10000

--- powerdeletesuite.js
+++ powerdeletesuite.js
@@ -909,7 +909,7 @@ var pd = {
           pd.task.after = pd.task.items[0].data.name;
           pd.actions.children.handleSingle();
         }
-      }, 30000);
+      }, 10000);
     },
     edit: function (item) {
       setTimeout(() => {
@@ -950,7 +950,7 @@ var pd = {
           pd.task.items[0].pdEdited = true;
           pd.actions.children.handleSingle();
         }
-      }, 30000);
+      }, 10000);
     },
   },
   ui: {
